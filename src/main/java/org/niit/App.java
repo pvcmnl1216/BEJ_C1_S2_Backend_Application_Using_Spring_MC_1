47b13dcf1b7b81ec1defc75d6fbf587fdf56fec3
Philip Meshach <Philipmeshach.pm@gmail.com>
2022-12-21 13:14:15 +0530

    get the result in main method

--- src/main/java/org/niit/App.java
+++ src/main/java/org/niit/App.java
@@ -1,5 +1,10 @@
 package org.niit;
 
+import org.niit.Domin.Movie;
+import org.niit.MovieConfig.MovieConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +14,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieConfig.class);
+        Movie mov = applicationContext.getBean(Movie.class);
+        System.out.println("mov = " + mov);
     }
 }
--- src/main/java/org/niit/MovieConfig/MovieConfig.java
+++ src/main/java/org/niit/MovieConfig/MovieConfig.java
@@ -6,9 +6,10 @@
 package org.niit.MovieConfig;
 
 import org.niit.Domin.Movie;
+import org.springframework.context.annotation.Bean;
 
 public class MovieConfig {
-
+    @Bean
     public Movie getDetails(){
         return new Movie("Avatar2","2","9.2");
     }
